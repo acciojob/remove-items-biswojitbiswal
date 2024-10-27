5d0c76c26e9f2b7709dd97b983da8453523b9a10
Biswojit Biswal <120778980+biswojitbiswal@users.noreply.github.com>
2024-10-27 10:26:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+let btn = document.querySelector("input[type='button']");
+let selctor = document.getElementById("colorSelect");
+
+btn.addEventListener("click", () => {
+	let selectIndex = selctor.selectedIndex;
+
+	if(selectIndex !== -1){
+		selctor.remove(selectIndex);
+	}
+})
